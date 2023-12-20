a36e7f9d5da0900a867fba72903ea83e38013058
Pedro Bueno <devbueno3@gmail.com>
2023-12-19 23:07:39 -0300

    Ajuste no envio de e-mails para lidar com falhas por timeout

--- app/Jobs/EnviarCurriculoJob.php
+++ app/Jobs/EnviarCurriculoJob.php
@@ -26,8 +26,30 @@ class EnviarCurriculoJob implements ShouldQueue
     {
         $emailDestino = 'devbueno3@gmail.com';
         
-        // Lógica de envio de e-mail
-        Mail::to($emailDestino)->send(new CurriculoEnviado($this->dataDb));
+        $maxAttempts = 3;
+        $attempts = 0;
+
+        while ($attempts < $maxAttempts) {
+            try {
+                // Lógica de envio de e-mail
+                Mail::to($emailDestino)->send(new CurriculoEnviado($this->dataDb));
+                
+                // Se chegou aqui, o e-mail foi enviado com sucesso
+                break;
+            } catch (\Exception $e) {
+                // Log do erro
+                Log::error('Erro ao enviar e-mail no Job: ' . $e->getMessage());
+
+                // Aguarde por um curto período antes de tentar novamente
+                sleep(2);
+                $attempts++;
+            }
+        }
+
+        if ($attempts === $maxAttempts) {
+            // Se atingir o número máximo de tentativas, trate conforme necessário
+            Log::error('Falha ao enviar e-mail após várias tentativas no Job.');
+        }
 
     }
 }
